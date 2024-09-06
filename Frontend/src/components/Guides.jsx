da02bec1641602908b68679b9a7e1fd2c205231a
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-09-06 23:23:30 +0530

    Some progress

--- Frontend/src/components/Guides.jsx
+++ Frontend/src/components/Guides.jsx
@@ -6,17 +6,17 @@ export default function Guides() {
       <div>
         <div
           id="controls-carousel"
-          className="relative w-full"
+          class="relative w-full"
           data-carousel="static"
         >
-          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <div className="destination-list">
+              <div class="destination-list">
                 <article>
                   <img
                     src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg"
                     alt="Paris"
-                    className="destination-image"
+                    class="destination-image"
                   />
                   <h3>Paris</h3>
                   <p>
@@ -33,33 +33,33 @@ export default function Guides() {
               </div>
             </div>
             <div
-              className="hidden duration-700 ease-in-out"
+              className=" duration-700 ease-in-out"
               data-carousel-item="active"
             >
               <img
-                src="/docs/images/carousel/carousel-2.svg"
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 alt="..."
               />
             </div>
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
               <img
-                src="/docs/images/carousel/carousel-3.svg"
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 alt="..."
               />
             </div>
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
               <img
-                src="..."
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 alt="..."
               />
             </div>
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
               <img
-                src="/docs/images/carousel/carousel-5.svg"
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/03/1c/9c.jpg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 alt="..."
               />
             </div>
@@ -69,9 +69,9 @@ export default function Guides() {
             className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-prev
           >
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
               <svg
-                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -79,13 +79,13 @@ export default function Guides() {
               >
                 <path
                   stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
                   d="M5 1 1 5l4 4"
                 />
               </svg>
-              <span className="sr-only">Previous</span>
+              <span class="sr-only">Previous</span>
             </span>
           </button>
           <button
@@ -93,9 +93,9 @@ export default function Guides() {
             className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-next
           >
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
               <svg
-                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -103,13 +103,13 @@ export default function Guides() {
               >
                 <path
                   stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
                   d="m1 9 4-4-4-4"
                 />
               </svg>
-              <span className="sr-only">Next</span>
+              <span class="sr-only">Next</span>
             </span>
           </button>
         </div>
